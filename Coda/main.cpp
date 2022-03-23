277b677a8c8c3bff9b27fde8fa0acc9c80af07b6
Daniele Guerra <d.guerra6@studenti.uniba.it>
2022-03-23 17:55:23 +0100

    Inserito main di prova

---
+++ Coda/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "coda.h"
+
+using namespace std;
+
+int main()
+{
+
+    coda<int> a;
+
+    a.incoda(3);
+
+    
+    cout << "Hello world!" << endl;
+    return 0;
+}
